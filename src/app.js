392fa98c07d9ac412c6fb436f432f7065e3453b0
Harsh Jain <harshjain04055@gmail.com>
2026-08-19 11:12:28 +0530

    Fix CORS policy for live frontend

--- src/app.js
+++ src/app.js
@@ -30,7 +30,9 @@ const app = express();
 
 // Middlewares
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
